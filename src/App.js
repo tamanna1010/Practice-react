e891f66439e022ddd56a6f16d7dc9b30625122c8
Tamanna <tamannas10110@gmail.com>
2024-03-14 22:45:56 +0530

    ADD : Added tailwind configuration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
+
 const App = () => {
   return (
     <>
-      <div className="App">Hey</div>
+      <div className="App">
+        Hey
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
     </>
   );
 };
